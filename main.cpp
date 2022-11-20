cb6730c6e2d484552faa26d374e5df772f28feec
Kartikeya <109892538+mightyoranje@users.noreply.github.com>
2022-11-21 00:43:53 +0530

    Update main.cpp
    The code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
-
-int main() {
-  cout << "Hello World";
-  return 0;
+int main(){
+    struct nodeType{
+    string info;                    
+    nodeType *link;
+    };
+    string str;
+    string lineFromFile;
+    ifstream myfile("./file.txt");
+    string line;
+    nodeType *first, *last, *newNode;
+    first = NULL;
+    last = NULL;
+    int n;
+    int count=0;
+    cout<<"Enter the number of lines in the file that you want to insert: ";
+    cin >> n;
+    while(getline(myfile,lineFromFile) && count <= n){
+        str = lineFromFile;
+        newNode = new nodeType;
+        newNode->info = str;
+        newNode->link = NULL;
+        cout << "line " << count << " inserted in the node."<<endl;
+        if(first == NULL){
+            first = newNode;
+            last = newNode;
+        }
+        else{
+            last->link = newNode;
+            last = newNode;
+        }
+        count=count + 1;
+    }
+    cout<<"\n";
+    cout<< "Output: ";
+    while(first != NULL){
+        cout<<first->info<<" ";
+        first = first->link;
+    }
+    return 0;
 }
